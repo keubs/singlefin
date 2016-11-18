26c63ef907591aef32e0e3503779659af4d9ca75
Kevin Cook <kevinac4@gmail.com>
2016-11-18 09:07:00 -0800

    scroll header

--- wp-content/themes/singlefin/header.php
+++ wp-content/themes/singlefin/header.php
@@ -34,24 +34,23 @@
                 </div>
             </div>
         </div>
-
-
-        <?php if (is_front_page()) { ?>
-        <!-- <div class="header-image">
-
-
-
-            <div class="header-image-text">
-                <h1>Welcome to <br/>Single Fin Studio.</h1>
-                <h2>Drawing lines <br/>since 1991.</h2>
-                <div class="loading"></div>
+    </div>
+    <div class="header main-header scroll">
+        <div class="header-top">
+            <div class="container" style="height: 45px; position: relative">
+                <!-- <a href="<?php echo SITE_URL; ?>" class="header-top-logo top"></a> -->
+                <a href="<?php echo SITE_URL; ?>" class="header-top-logo"></a>
+                <div class="header-top-menu home">
+                    <a class="header-top-menu-link" href="#work">Work</a>
+                    <a class="header-top-menu-link" href="#about">About</a>
+                    <a class="header-top-menu-link" href="#contact">Contact</a>
+                </div>
+                <div class="header-top-menu single-page">
+                    <a class="header-top-menu-link" href="<?php echo SITE_URL; ?>#work">Work</a>
+                    <a class="header-top-menu-link" href="<?php echo SITE_URL; ?>#about">About</a>
+                    <a class="header-top-menu-link" href="#contact">Contact</a>
+                </div>
             </div>
-
-            <div class="header-image-down-container">
-                <a href="#work" class="header-image-down bounce"><img src="<?php echo THEME_DIR . '/img/arrow.svg'; ?>"/></a>
-            </div>
-        </div> -->
-        <?php } ?>
-
+        </div>
     </div>
 
