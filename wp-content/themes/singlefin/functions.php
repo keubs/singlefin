19deb479366029dcfe56a4f765f8ffc0d34f1c50
Kevin Cook <kevinac4@gmail.com>
2016-11-18 12:55:44 -0800

    font faces to css

--- wp-content/themes/singlefin/functions.php
+++ wp-content/themes/singlefin/functions.php
@@ -12,8 +12,8 @@ function enqueue_styles() {
 	wp_register_style( 'style', THEME_DIR . '/style.css', array(), '1', 'all' );
 	wp_enqueue_style( 'style' );
 
-    wp_register_style( 'style', THEME_DIR . '/fonts/archer/stylesheet.css', array(), '1', 'all' );
-    wp_enqueue_style( 'archer_style' );
+    // wp_register_style( 'style', THEME_DIR . '/fonts/archer/stylesheet.css', array(), '1', 'all' );
+    // wp_enqueue_style( 'archer_style' );
 
 }
 	add_action( 'wp_enqueue_scripts', 'enqueue_styles' );
