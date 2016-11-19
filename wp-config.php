a0bb389cf94a5481db27f3a35c45839ea01c473f
Kevin Cook <kevinac4@gmail.com>
2016-11-19 03:51:59 +0000

    uploads

--- wp-config.php
+++ wp-config.php
@@ -16,13 +16,13 @@
 
 // ** MySQL settings - You can get this info from your web host ** //
 /** The name of the database for WordPress */
-define('DB_NAME', 'singlefin');
+define('DB_NAME', 'singlefindb');
 
 /** MySQL database username */
-define('DB_USER', 'root');
+define('DB_USER', 'keubs_bbdrupal');
 
 /** MySQL database password */
-define('DB_PASSWORD', 'root');
+define('DB_PASSWORD', 'h8faJ4CbHLnRAx2u');
 
 /** MySQL hostname */
 define('DB_HOST', 'localhost');
